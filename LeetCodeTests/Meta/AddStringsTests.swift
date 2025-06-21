ce9c31b8d977939de332540b3f356da7685f1a8d
ives-algorhym <algoives@icloud.com>
2025-06-20 18:45:57 -0700

    Implement AddString

--- LeetCodeTests/Meta/AddStringsTests.swift
+++ LeetCodeTests/Meta/AddStringsTests.swift
@@ -5,4 +5,34 @@
 //  Created by Ives Murillo on 6/18/25.
 //
 
-import Foundation
+@testable import LeetCode
+import XCTest
+
+final class AddStringsTests: XCTestCase {
+    func test_addString_basicAddition() {
+        // Arrange
+        let sut = AddString()
+        // Act
+        let result = sut.addString("2", "3")
+        // Assert
+        XCTAssertEqual(result, "5")
+    }
+
+    func test_addString_singleDigitWithCarry() {
+        // Assert
+        let sut = AddString()
+        // Act
+        let result  = sut.addString("7", "8")
+        // Assert
+        XCTAssertEqual(result, "15")
+    }
+
+    func test_addString_differentLengths() {
+        // Arrange
+        let sut = AddString()
+        // Act
+        let result = sut.addString("9", "99")
+        // Assert
+        XCTAssertEqual(result, "108")
+    }
+}
