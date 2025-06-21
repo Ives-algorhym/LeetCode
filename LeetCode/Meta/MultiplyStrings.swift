d669b6984a05e228a402e089cdeec7e308bb7b9d
ives-algorhym <algoives@icloud.com>
2025-06-20 18:46:47 -0700

    Implement MultiplyString

--- LeetCode/Meta/MultiplyStrings.swift
+++ LeetCode/Meta/MultiplyStrings.swift
@@ -5,4 +5,68 @@
 //  Created by Ives Murillo on 6/19/25.
 //
 
-import Foundation
+final class MultiplyStrings {
+    func multiplyOneDigit(_ firstNumber: String, _ secondDigit: Character, _ numZeros: Int) -> [Int] {
+        var result = Array(repeating: 0, count: numZeros)
+        var carry = 0
+
+        for digitChar in firstNumber {
+            let product = (secondDigit.wholeNumberValue ?? 0) * (
+                digitChar.wholeNumberValue ?? 0
+            ) + carry
+            result.append(product % 10)
+            carry = product / 10
+        }
+
+        if carry > 0 {
+            result.append(carry)
+        }
+
+        return result
+    }
+
+    func sumResults(_ results: inout [[Int]]) -> String {
+        var answer = results.removeLast()
+
+        for result in results {
+            var newAnswer: [Int] = []
+            var carry = 0
+            let maxLength = max(answer.count, result.count)
+
+            for i in 0..<maxLength {
+                let digit1 = i < result.count ? result[i] : 0
+                let digit2 = i < answer.count ? answer[i] : 0
+                let total = digit1 + digit2 + carry
+                newAnswer.append(total % 10)
+                carry = total / 10
+            }
+
+            if carry > 0 {
+                newAnswer.append(carry)
+            }
+
+            answer = newAnswer
+        }
+
+        return answer.map(String.init).joined()
+    }
+
+    func multiply(_ num1: String, _ num2: String) -> String {
+        if num1 == "0" || num2 == "0" {
+            return "0"
+        }
+
+        let firstReversed = String(num1.reversed())
+        let secondReversed = String(num2.reversed())
+
+        var results: [[Int]] = []
+
+        for (i, digit) in secondReversed.enumerated() {
+           let product = multiplyOneDigit(firstReversed, digit, i)
+            results.append(product)
+        }
+
+        var answer = sumResults(&results)
+        return String(answer.reversed())
+    }
+}
--- LeetCodeTests/Meta/MultiplyStringsTests.swift
+++ LeetCodeTests/Meta/MultiplyStringsTests.swift
@@ -5,4 +5,46 @@
 //  Created by Ives Murillo on 6/19/25.
 //
 
-import Foundation
+@testable import LeetCode
+import XCTest
+
+final class MultiplyTests: XCTestCase {
+    func test_multiplyOneDigit_shouldReturnZero() {
+        // Arrange
+        let sut = MultiplyStrings()
+        let num1Reversed = "32"
+        let digit: Character = "2"
+        let i = 0
+        // Act
+        let result = sut.multiplyOneDigit(num1Reversed, digit, i)
+        // Assert
+        XCTAssertEqual(result, [6, 4])
+    }
+
+    func test_multiplyWithZero_shouldReturnZero() {
+        // Arrange
+        let sut = MultiplyStrings()
+        // Act
+        let result = sut.multiply("24", "0")
+        // Assert
+        XCTAssertEqual(result, "0")
+    }
+
+    func test_multiplyWithOne_shouldReturnSame() {
+        // Arrange
+        let sut = MultiplyStrings()
+        // Act
+        let result = sut.multiply("24", "1")
+        // Assert
+        XCTAssertEqual(result, "24")
+    }
+
+    func test_multiplyWithOne_shouldReturnSameNumber() {
+        // Arrange
+        let sut = MultiplyStrings()
+        // Act
+        let result = sut.multiply("1", "3")
+        // Assert
+        XCTAssertEqual(result, "3")
+    }
+}
